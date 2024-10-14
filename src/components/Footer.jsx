df64b4a65f7bd23d35466cc38d0fb309d5ba7dfb
Sofia Ex <dev.exarchou@outlook.com>
2024-10-14 14:25:05 +0200

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export const Footer = () =>
 
     <footer className="fixed bottom-0 left-0 w-full bg-sky-700 text-white">
         <div className="flex justify-between items-center w-full px-4 py-2">
-            <p>&copy; 2024 Mystery Game. All rights reserved.</p>
+            <p>&copy; 2024 Mystery Number. All rights reserved.</p>
             <div className="flex items-center">
                 <p className="mr-4">Connect with us on:</p>
                 <ul className="social-icons flex space-x-4">
