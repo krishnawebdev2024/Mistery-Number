23d30f88d5a6f7f050f4e2dd191bd50ebe5aab50
Sofia Ex <dev.exarchou@outlook.com>
2024-10-14 14:47:26 +0200

    fix import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { NavBar } from "./components/NavBar";
-import Footer from "./components/Footer";
+import { Footer } from "./components/Footer";
 import Home from "./components/Home";
 import Gamelogic from "./components/Gamelogic"; // Adjust the path based on your folder structure
 
